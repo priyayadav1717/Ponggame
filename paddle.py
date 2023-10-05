9ccf9ef1d962c30fabcdaa144c667afa451ed449
Priya Yadav <priyayadav1344@gmail.com>
2023-10-05 19:17:12 +0530

    first commit

---
+++ paddle.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.penup()
+        self.goto(position)
+        self.shapesize(stretch_wid=5, stretch_len=1, outline=None)
+
+    def paddle_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def paddle_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
